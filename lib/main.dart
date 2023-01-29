1f6c5e70ef2a65ff126e8d7400fb66d078f81a00
diego <perezdi1@msu.edu>
2023-01-29 05:32:46 -0500

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zen_you/tutorial.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main(){
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
